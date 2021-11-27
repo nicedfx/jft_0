dc99cc85e7327ec92b968c5f2b6573fc46a89ad6
nicedfx <nicedfx@gmail.com>
2021-11-27 22:02:19 +0300

    Add three tests for the Point class

---
+++ sandbox/src/test/java/com/github/nicedfx/sandbox/PointsTests.java
@@ -0,0 +1,31 @@
+package com.github.nicedfx.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointsTests {
+
+    @Test
+    public void testPointFirst() {
+        Point p1 = new Point(6, 12);
+        Point p2 = new Point(7, 2);
+
+        Assert.assertEquals(p1.distance(p2), 10.04987562112089);
+    }
+
+    @Test
+    public void testPointSecond() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 1);
+
+        Assert.assertEquals(p1.distance(p2), 0.0);
+    }
+
+    @Test
+    public void testPointThird() {
+        Point p1 = new Point(1, 100);
+        Point p2 = new Point(100, 1);
+
+        Assert.assertEquals(p1.distance(p2), 140.0071426749364);
+    }
+}
