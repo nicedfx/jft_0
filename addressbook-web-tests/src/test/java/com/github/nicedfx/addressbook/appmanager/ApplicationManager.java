be19c81819d18b57676d14cfd1fbdb5df70a5512
nicedfx <nicedfx@gmail.com>
2021-12-11 16:32:19 +0300

    Add possibility to run in FireFox, Edge, Chrome

--- addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package com.github.nicedfx.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,20 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactsHelper contactsHelper;
+    private Browser browser;
+
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(Browser.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser == Browser.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == Browser.EDGE){
+            wd = new EdgeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/edit.php");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package com.github.nicedfx.addressbook.tests;
 
 import com.github.nicedfx.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(Browser.EDGE);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
