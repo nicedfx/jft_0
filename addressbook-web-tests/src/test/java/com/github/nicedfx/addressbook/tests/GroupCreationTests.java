c1bcdebd08bf976f7fe5e1dba1d6c1d31d49e9a7
nicedfx <nicedfx@gmail.com>
2022-01-16 20:30:26 +0300

    Parametrize GroupCreationTests

--- addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/tests/GroupCreationTests.java
@@ -2,20 +2,33 @@ package com.github.nicedfx.addressbook.tests;
 
 import com.github.nicedfx.addressbook.model.GroupData;
 import com.github.nicedfx.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
-    @Test
-    public void testGroupCreation() {
+
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
-
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
-
         Groups after = app.group().all();
 
         assertThat(after.size(), equalTo(before.size() + 1));
