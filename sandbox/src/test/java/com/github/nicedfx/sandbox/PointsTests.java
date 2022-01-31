c0cf0a5fe09fa8e9372060dcc6760cae11b56f39
nicedfx <nicedfx@gmail.com>
2022-01-31 16:08:39 +0300

    Break one of the sandbox's tests

--- sandbox/src/test/java/com/github/nicedfx/sandbox/PointsTests.java
+++ sandbox/src/test/java/com/github/nicedfx/sandbox/PointsTests.java
@@ -18,7 +18,7 @@ public class PointsTests {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(1, 1);
 
-        Assert.assertEquals(p1.distance(p2), 0.0);
+        Assert.assertEquals(p1.distance(p2), 1.0);
     }
 
     @Test
