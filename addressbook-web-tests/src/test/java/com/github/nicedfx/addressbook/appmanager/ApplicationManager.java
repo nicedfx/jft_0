a684cf9eb08a6dd91762ead083e62ece024b12ac
nicedfx <nicedfx@gmail.com>
2022-01-31 18:01:08 +0300

    Add possibility to run tests on different platforms

--- addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/com/github/nicedfx/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package com.github.nicedfx.addressbook.appmanager;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -46,6 +47,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities  capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
